f41f1f6a974444d190b72526a99b5e866fa98d74
M7139 <mohamedaljoamee@gmail.com>
2025-08-04 15:12:03 +0300

    added the nav bar and the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './components/Nav'
+import Ride from './components/Ride'
+import RideDetail from './components/RideDetail'
+import Category from './pages/Category'
+import Home from './pages/Home'
+import { Route, Router, Routes } from 'react-router-dom'
 
 const App = () => {
-  return <></>
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/RideForm.jsx
@@ -0,0 +1,6 @@
+import { useNavigate } from "react-router-dom"
+
+const RideForm = () => {
+  return <></>
+}
+export default RideForm
